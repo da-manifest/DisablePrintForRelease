d4ee337ced10f86760422c7a554a4cd1508ab0fa
Admin <Admin@vl2003.ar14-24.ekb.ru.mirasystem.net>
2017-05-15 10:16:38 +0500

    -init commit

---
+++ DisablePrintForRelease.swift
@@ -0,0 +1,20 @@
+//
+//  DisablePrintForRelease.swift
+//  CallRecorder
+//
+//  Created by Admin on 15.05.17.
+//  Copyright © 2017 Intehno. All rights reserved.
+//
+
+
+// uncomment for release, comment for debug
+
+public func debugPrint(_ items: Any..., separator: String = " ", terminator: String = "\n")
+{
+
+}
+
+public func print(_ items: Any..., separator: String = " ", terminator: String = "\n")
+{
+
+}
